f8ecea9be7a43df6a4b9882279f6303347671b15
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-05-14 13:25:54 -0300

    triangulo base*altura

--- src/triangulo.ts
+++ src/triangulo.ts
@@ -1,14 +1,17 @@
+function calcularAreaTriangulo(Base: number, Altura: number): number {
+  let Area = (Base * Altura) / 2;
+  return Area;
+}
+let base: number = 1;
+let altura: number = 2;
+
+for (let indice = 0; indice <= 14; indice++) {
+  console.log(calcularAreaTriangulo(base, altura));
+  base += 1;
+  altura += 2;
+}
+
 //Realice un programa que devuelva el área del triángulo usando los siguientes pares de base-altura:
 //(1,2) (2,4) (3,6) (4,8) (5, 10) (6,12) (7,14)
 //Implemente un método llamado calcularAreaTriangulo que reciba dos números por parámetro (uno llamado base y otro altura)
 //(base*altura)/2
-let base: number = 1;
-let altura: number = 2;
-let area: number;
-area = (base * altura) / 2;
-
-function dibujar_triangulo(base: number = 0, altura: number = 2): number {
-  return area;
-}
-for (base = 0; index < array.length; index++) {}
-console.log(dibujar_triangulo(area));
