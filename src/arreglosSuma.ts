8c9d8222e51fe5c8d9a6620e84c06e925d6b1595
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-05-22 21:17:11 -0300

    arreglo suma corrección de variable suma

--- src/arreglosSuma.ts
+++ src/arreglosSuma.ts
@@ -1,6 +1,5 @@
 let v1: number[] = new Array(6);
 let v2: number[] = new Array(6);
-let suma: number[] = new Array(6);
 function ingresar_numeros() {
   for (let indice: number = 0; indice < 6; indice++) {
     v1[indice] = Number(prompt("Ingrese numero1: "));
@@ -10,6 +9,7 @@ function ingresar_numeros() {
   }
 }
 function sumar() {
+  let suma: number[] = new Array(6);
   for (let indice: number = 0; indice < 6; indice++) {
     suma[indice] = v1[indice] + v2[indice];
     console.log("El número: ", v1[indice], "+", v2[indice], "=", suma[indice]);
